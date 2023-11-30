defd2f695dc97bde54b87d20fdf1353c80862cc8
corwin-bell <corwin.m.bell@gmail.com>
2023-11-30 12:21:09 -0700

    declare constants and functions in prep for game play

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,29 @@
 
+// declare constants
+const rpsArray = ['rock','paper','scissors']
 // declare functions
 //     getComputerChoice
 //         randomly generate choice between rock, paper, or scissors
 //         assign choice to computerSelection variable
 //         return computerSelection
-
+function getComputerChoice() {
+    const computerSelection = rpsArray[Math.floor(Math.random() * rpsArray.length)]
+    return computerSelection
+}
 //     getPlayerChoice
 //         prompt Player to enter case-insensitive option of rock, paper, or scissors
 //         if Player enters something else, ask for rock, paper, or scissors
 //         assign choice to playerSelection
 //         return computerSelection
-
+function getPlayerChoice() {
+    const playerSelection = prompt('please enter rock, paper, or scissors','').toLowerCase()
+    if (rpsArray.includes(playerSelection)) {
+        return playerSelection
+    }
+    else {
+        getPlayerChoice()
+    }
+}
 //     playRound(playerSelection, computerSelection)
 //         case if playerSelection and computerSelection tie
 //             return string "Tie, play again. playerSelection ties computerSelection."
@@ -21,5 +34,6 @@
 
 // call functions
 //     computerSelection = getComputerChoice()
+const playerSelection = getPlayerChoice()
 //     playerSelection = getPlayerChoice()
 //     alert(playRound(playerSelection, computerSelection))
