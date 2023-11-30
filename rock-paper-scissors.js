cd73e2fe386ddcbcf10828b5f327ce62c3c9b57a
corwin-bell <corwin.m.bell@gmail.com>
2023-11-30 11:57:59 -0700

    created initial algorithm plan for rock paper scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,25 @@
+
+// declare functions
+//     getComputerChoice
+//         randomly generate choice between rock, paper, or scissors
+//         assign choice to computerSelection variable
+//         return computerSelection
+
+//     getPlayerChoice
+//         prompt Player to enter case-insensitive option of rock, paper, or scissors
+//         if Player enters something else, ask for rock, paper, or scissors
+//         assign choice to playerSelection
+//         return computerSelection
+
+//     playRound(playerSelection, computerSelection)
+//         case if playerSelection and computerSelection tie
+//             return string "Tie, play again. playerSelection ties computerSelection."
+//         case if playerSelection beats computerSelection
+//             return string "You Win! playerSelection beats computerSelection."
+//         case if playerSelection loses to computerSelection
+//             return string "You Lose! computerSelection beats playerSelection"
+
+// call functions
+//     computerSelection = getComputerChoice()
+//     playerSelection = getPlayerChoice()
+//     alert(playRound(playerSelection, computerSelection))
