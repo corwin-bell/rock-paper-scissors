c2e59258b2cf4449615b4ee0e2e725993b1ba0d5
corwin-bell <corwin.m.bell@gmail.com>
2023-11-30 12:58:24 -0700

    declared and called functions to test game play

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,29 +1,35 @@
 
-// declare constants
-const rpsArray = ['rock','paper','scissors']
+// declare variables
+const rpsArray = ['rock','paper','scissors'];
+let computerSelection = '';
+let playerSelection = '';
+let gameMessage = '';
+
 // declare functions
 //     getComputerChoice
 //         randomly generate choice between rock, paper, or scissors
 //         assign choice to computerSelection variable
 //         return computerSelection
 function getComputerChoice() {
-    const computerSelection = rpsArray[Math.floor(Math.random() * rpsArray.length)]
-    return computerSelection
+    computerSelection = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    return computerSelection;
 }
+
 //     getPlayerChoice
 //         prompt Player to enter case-insensitive option of rock, paper, or scissors
 //         if Player enters something else, ask for rock, paper, or scissors
 //         assign choice to playerSelection
 //         return computerSelection
 function getPlayerChoice() {
-    const playerSelection = prompt('please enter rock, paper, or scissors','').toLowerCase()
+    playerSelection = prompt('please enter rock, paper, or scissors','').toLowerCase();
     if (rpsArray.includes(playerSelection)) {
-        return playerSelection
+        return playerSelection;
     }
     else {
-        getPlayerChoice()
+        getPlayerChoice();
     }
 }
+
 //     playRound(playerSelection, computerSelection)
 //         case if playerSelection and computerSelection tie
 //             return string "Tie, play again. playerSelection ties computerSelection."
@@ -31,9 +37,20 @@ function getPlayerChoice() {
 //             return string "You Win! playerSelection beats computerSelection."
 //         case if playerSelection loses to computerSelection
 //             return string "You Lose! computerSelection beats playerSelection"
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        gameMessage = `Tie, play again. ${playerSelection} ties ${computerSelection}.`;
+    }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        gameMessage = `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    else {
+        gameMessage = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    }
+    return gameMessage
+}
 // call functions
-//     computerSelection = getComputerChoice()
-const playerSelection = getPlayerChoice()
-//     playerSelection = getPlayerChoice()
-//     alert(playRound(playerSelection, computerSelection))
+alert(playRound(getPlayerChoice(), getComputerChoice()));
